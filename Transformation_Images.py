5d1020c5fab7a4f565f60b90cbcba59ee3176d28
CyrineBenHammou <cyrine.bhammou@outlook.com>
2023-05-13 02:18:43 +0100

    Updated to last version

--- Transformation_Images.py
+++ Transformation_Images.py
@@ -1,19 +1,20 @@
 from sklearn.base import BaseEstimator,TransformerMixin
 from skimage import color
-import matplotlib.pyplot as plt
 import numpy as np
+from skimage.io import imread
 
 class TransformationImageVoiture(BaseEstimator,TransformerMixin):
     def __init__(self) :
         return None
     
-    def fit(self, X, y=None):
-        X_=X.copy()
+    def fit(self, X_, y=None):
+        
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X_, y=None):
 
-        X_=X.copy()
+       
+        X_ = imread(X_)
         X_ = color.rgb2gray(X_)
         X_ = np.resize(X_, (20,20))
         X_= X_.reshape(20*20)
