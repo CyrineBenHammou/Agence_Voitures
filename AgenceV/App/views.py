49163a0df1eccee7ec104c1176aaf85a302dcd26
CyrineBenHammou <cyrine.bhammou@outlook.com>
2023-05-14 17:25:43 +0100

    del

--- AgenceV/App/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def App(request):
-  template = loader.get_template('Home.html')
-  return HttpResponse(template.render())
